c0e0b321673e9cf0fb22bed5e6f20df142452f3b
Alexander Robinson <alexander.robinson@awi.de>
2026-01-20 10:23:13 +0100

    Updated docs.

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,13 @@
-using Documenter, ModelAnalysis
+using Documenter
+using ModelAnalysis
 
-makedocs(sitename="ModelAnalysis.jl")
+makedocs(
+    sitename = "ModelAnalysis.jl",
+    modules = [ModelAnalysis],
+    pages = [
+        "Home" => "index.md",
+    ],
+)
 
 deploydocs(
     repo = "github.com/fesmc/ModelAnalysis.jl.git",
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,1 +1,26 @@
 # ModelAnalysis.jl Documentation
+# ModelAnalysis.jl
+
+Tools for analysing model output in Julia.
+
+## Installation
+
+```julia
+using Pkg
+Pkg.add("ModelAnalysis")
+```
+
+## Quick example
+
+```julia
+using ModelAnalysis
+
+# example usage here
+
+TO DO
+```
+
+## API
+
+```@autodocs
+Modules = [ModelAnalysis]
